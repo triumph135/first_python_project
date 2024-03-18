4761a178a1a51af2ca4f533521893556b1e5271a
triumph135 <shaneflemingllc@gmail.com>
2024-03-18 14:08:08 +0000

    Added the Flask project

---
+++ app.py
@@ -0,0 +1,32 @@
+# print("Hello, world. This is Python!")
+
+# name = input ("What is your name?") 
+# print ("Hello, " + name)
+
+
+# name = input ("What is your name?")
+# age = input ("How old are you?")
+# gender = input.upper ("Are you a male or female?")
+# marital_status = input ("Are you married?")  
+
+# if gender == "MALE":
+#   print ("Hello, Mr. " + name)  
+# elif gender == "FEMALE":
+#   if marital_status == "yes":
+#     print ("Hello, Mrs. " + name)
+#   else : 
+#     print ("Hello, Ms. " + name)
+    
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, Shane!</p>"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
